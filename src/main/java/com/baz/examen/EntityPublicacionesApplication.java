87fec929cefd50b633d63fce84a2ec208a105696
enrique728811 <enrique728811@gmail.com>
2022-07-13 23:37:38 -0500

    Se sube carga inicial

---
+++ src/main/java/com/baz/examen/EntityPublicacionesApplication.java
@@ -0,0 +1,13 @@
+package com.baz.examen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntityPublicacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntityPublicacionesApplication.class, args);
+	}
+
+}
